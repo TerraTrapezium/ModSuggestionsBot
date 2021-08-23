910de00cd87cdc008911b84bb6dabf66fcc38e3c
Charlie <45357714+GoodPro712@users.noreply.github.com>
2021-08-23 13:17:13 +1000

    Initial commit

---
+++ react.py
@@ -0,0 +1,16 @@
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+
+    async def on_message(self, message):
+        if message.channel.id == 866299513476349962:
+            await message.add_reaction("⬆")
+            await message.add_reaction("⬇")
+
+client = MyClient()
+client.run(config.get("TOKEN"))
