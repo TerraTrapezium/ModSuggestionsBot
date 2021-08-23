544ea0f895c1c01480e1a6b1983880954c714f6a
Charlie <45357714+GoodPro712@users.noreply.github.com>
2021-08-23 13:42:18 +1000

    Fix channel id check

--- react.py
+++ react.py
@@ -8,7 +8,7 @@ class MyClient(discord.Client):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
 
     async def on_message(self, message):
-        if message.channel.id == 866299513476349962:
+        if message.channel.id == int(config.get("CHANNEL_ID")):
             await message.add_reaction("⬆")
             await message.add_reaction("⬇")
 
